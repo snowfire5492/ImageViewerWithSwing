5a7e652de8dac351d7ad3890b328648526ae24ed
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2017-05-30 10:10:10 -0700

    update class

--- ImageViewerWithScrollBar.java
+++ ImageViewerWithScrollBar.java
@@ -1,1 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class ImageViewerWithScrollBar extends JFrame implements 
+ActionListener {
+
+	
+	public ImageViewerWithScrollBar(){
+		
+		setSize(1000,1000);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setTitle("ImageViewer");
+		
+		setIconImage(new 
+ImageIcon("C://Users//Eric//Desktop//Programs//CS245_JavaSwingGUI//ImageViewerIcon.png").getImage());
+		
+		
+		ImageIcon img = new 
+ImageIcon("C://Users//Eric//Desktop//Programs//CS245_JavaSwingGUI//OregonMap.png");
+		JLabel piclbl = new JLabel(img);
+		
+		
+		JScrollPane pane = new JScrollPane(piclbl);
+		
+		add(pane);
+		
+		//pack();
+		
+		
+		
+	}
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(()->
+		new ImageViewerWithScrollBar());
+	}
+	
+	
+}
 
